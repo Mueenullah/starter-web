c8f750548c1544e77a313c0ea1bb47bce75c80bd
mueenullah <mueen@uconnect.pk>
2019-01-04 14:15:49 +0500

    changes

---
+++ js/app.js
@@ -0,0 +1,52 @@
+let subjects = document.getElementById('subjects');
+subjects.style.listStyle='none';
+subjects.style.fontSize='2rem';
+subjects.style.color='red';
+subjects.style.padding='4rem';
+subjects.style.borderColor='black';
+subjects.style.display='flex';
+
+
+
+let byname = document.getElementsByClassName('lists')[1];
+byname.style.color='green';
+byname.style.paddingLeft='1rem';
+
+let byname1 = document.getElementsByClassName('lists')[2];
+byname1.style.color='blue';
+byname1.style.paddingLeft='1rem';
+
+let byname2 = document.getElementsByClassName('lists')[3];
+byname2.style.color='brown';
+byname2.style.paddingLeft='1rem';
+
+
+
+let byquery = document.querySelector('ul');
+byquery.style.fontSize='3rem';
+byquery.style.paddingLeft='20rem';
+byquery.style.margin='2rem';
+byquery.style.wordSpacing='3rem';
+byquery.style.border='3px solid black';
+
+
+let section2 = document.getElementsByTagName('div')[1];
+// section2.style.backgroundColor='green';
+section2.style.height='30rem';
+section2.style.border='3px solid black';
+section2.style.borderRadius='2rem';
+
+let z = document.querySelectorAll(".footer");
+z[0].style.display = "flex";
+z[0].style.justifyContent = "center";
+z[0].style.listStyle = "none";
+z[0].style.fontSize = "2rem";
+z[0].style.padding = "9rem";
+z[0].style.justifyContent="space-between";
+
+
+let pra = document.getElementsByTagName('p')[0];
+pra.style.fontSize='1.5rem';
+pra.style.color='gray';
+pra.style.textAlign='center';
+
---
+++ js/main.js
@@ -0,0 +1,66 @@
+
+// let clossingtag = document.querySelector('input');
+// x = clossingtag.getAttribute('name');
+// y = clossingtag.getAttribute('id');
+// z = clossingtag.getAttribute('type');
+
+// alert(z);
+// alert(y);
+// alert(x);
+//  e = clossingtag.hasAttribute('name');
+//  f = clossingtag.hasAttribute('id');
+//  g = clossingtag.hasAttribute('type');
+// alert(f);
+// alert(e);
+// alert(g);
+
+// l=clossingtag.setAttribute('id','name');
+// n=clossingtag.setAttribute('class','name');
+// m = clossingtag.getAttribute('id');
+// n = clossingtag.getAttribute('class');
+// alert(m);
+// alert(n);
+
+// f = clossingtag.removeAttribute('id');
+// g = clossingtag.removeAttribute('class');
+// h = clossingtag.removeAttribute('type');
+
+
+
+// let anchor = document.querySelector('a');
+// // a = anchor.getAttribute('href');
+// // b = anchor.getAttribute('id');
+// // c = anchor.getAttribute('class');
+// // alert(c);
+// // alert(a);
+// // alert(b);
+//   h = anchor.hasAttribute('href');
+//   j = anchor.hasAttribute('id');
+//   k = anchor.hasAttribute('class');
+//   alert(h);
+//   alert(j);
+//   alert(k);
+
+// o = anchor.setAttribute('id','anchor');
+// p = anchor.setAttribute('name','anchor');
+// q = anchor.setAttribute('href',"www.google.pk")
+
+// alert(div.getAttribute('style'));
+// alert(div.getAttribute('id'));
+// alert(div.getAttribute('class'));
+
+// alert(div.hasAttribute('style'))
+// alert(div.hasAttribute('id'))
+// alert(div.hasAttribute('class'))
+
+
+// alert(div.setAttribute('class', 'anchor'))
+// alert(div.setAttribute('id', 'anchor'))
+// alert(div.setAttribute('name', 'anchor'))
+
+// div.removeAttribute('class');
+// alert(div.getAttribute('class'));
+// div.removeAttribute('id');
+// alert(div.getAttribute('id'));
+// div.removeAttribute('name');
+// div(div.getAttribute('name'));
